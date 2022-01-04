063919925b16a8d1a77984c8a38deb6c298d124a
burkero2 <ronanburke@codeinstitute.net>
2022-01-04 16:41:41 +0000

    Deployment Commit

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+os.environ["DATABASE_URL"] = "postgres://lzjoxqhqpfgkwq:8354704885424ab94450330c07a8c47b9aff77df66ee22f506a75c99618acbf3@ec2-79-125-123-149.eu-west-1.compute.amazonaws.com:5432/dbll1i4hdr14d8"
+os.environ["SECRET_KEY"] = "MySecretKey101!!!!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://913247593199284:qVZ7dRVXFEuRS7xHbthfOJRDpGU@dizpdydnq"
